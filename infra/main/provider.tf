f7e3fe40548e45af63f0384bf01f67ca3441adc9
Mahesh Babu Miriyampalli <jesuscyrilpaul@gmail.com>
2024-02-09 02:55:12 +0530

    removed main & variable file, adding files in a proper manner

---
+++ infra/main/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+   region     = "eu-central-1"
+   access_key = "<INSERT_YOUR_ACCESS_KEY>"
+   secret_key = "<INSERT_YOUR_SECRET_KEY>"
+}
--- main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "example" {
-  ami = "ami-0123456789abcdef0" # Change the AMI 
-  instance_type = "t2.micro"
-}
