f76c52750ccb7a5d1d3d59049f4dcb25fbddcbb6
Mahesh Babu Miriyampalli <jesuscyrilpaul@gmail.com>
2024-02-09 04:39:27 +0530

    added locals folder

---
+++ infra/Locals/combine-locals-variables.tf
@@ -0,0 +1,16 @@
+variable location {
+  description = "Location of server"
+  type        = string
+  default     = "finland"
+}
+
+variable server_name {
+  description = "Name of server"
+  type        = string
+  default     = "primary-app-server"
+}
+
+
+locals {
+    server_details = "${var.location}-${var.server_name}"
+}
