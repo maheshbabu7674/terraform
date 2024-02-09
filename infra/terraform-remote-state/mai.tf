916ae96e209b26787b2a1a8b8896cb61fe1f666f
maheshbabu7674 <112943607+maheshbabu7674@users.noreply.github.com>
2024-02-09 23:07:47 +0530

    Update mai.tf

--- infra/terraform-remote-state/mai.tf
+++ infra/terraform-remote-state/mai.tf
@@ -1,1 +1,23 @@
+terraform {
+  backend "s3" {
+    bucket = "mahesh-terraform"
+    key    = "key/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 
+provider "aws" {
+   region     = "eu-central-1"
+   access_key = "<INSERT_YOUR_ACCESS_KEY>"
+   secret_key = "<INSERT_YOUR_SECRET_KEY>"
+}
+
+resource "aws_instance" "ec2_example" {
+
+   ami           = "ami-0767046d1677be5a0"
+   instance_type =  var.instance_type
+
+   tags = {
+           Name = "Terraform EC2"
+   }
+}
